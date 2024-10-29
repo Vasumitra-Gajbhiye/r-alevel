304c8b4dcc46065a1c49049b7c71b08450a16235
Vasumitra-Gajbhiye <vasumitragajbhiye20@gmail.com>
2024-10-29 18:56:23 +0530

    33

--- app/(others)/certificates/action.tsx
+++ app/(others)/certificates/action.tsx
@@ -3,6 +3,7 @@
 import { redirect } from "next/navigation";
 
 export async function navigate(data: any) {
-  console.log(`redirecting to http://localhost:3000/certificates/${data}`);
-  redirect(`http://localhost:3000/certificates/${data}`);
+  // console.log(`redirecting to http://localhost:3000/certificates/${data}`);
+  // redirect(`http://localhost:3000/certificates/${data}`);
+  redirect(`https://r-alevel.netlify.app/certificates/${data}`);
 }
