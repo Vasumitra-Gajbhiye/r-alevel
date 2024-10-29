b1f44c7ce19520452603fab86b85d41fda788026
Vasumitra-Gajbhiye <vasumitragajbhiye20@gmail.com>
2024-10-29 18:49:17 +0530

    chnge 2

--- app/(others)/resources/action.tsx
+++ app/(others)/resources/action.tsx
@@ -3,5 +3,6 @@
 import { redirect } from "next/navigation";
 
 export async function navigate(data: any) {
-  redirect(`http://localhost:3000/resources/${data}`);
+  // redirect(`http://localhost:3000/resources/${data}`);
+  redirect(`https://r-alevel.netlify.app/resources/${data}`);
 }
