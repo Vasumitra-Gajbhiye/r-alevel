1a96ace26453f3fb82c8f8839741725ced7a77f5
Vasumitra-Gajbhiye <vasumitragajbhiye20@gmail.com>
2025-03-23 11:17:04 +0530

    nwefj

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -11,7 +11,7 @@ const poppins = Poppins({
 
 export const metadata: Metadata = {
   title: "r/alevel",
-  description: "World's Largest A-Level Community",
+  description: "World's Largest A-Level Community new new",
   // keywords:
   // "alevel, a-level, a level, alevel community, a-level community, cambridge, edexcel",
 
