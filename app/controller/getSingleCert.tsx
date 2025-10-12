014adbc90d9664a411a0d34acfd98dabdbfec637
vasu <vasumitragajbhiye20@gmail.com>
2025-10-12 15:52:33 +0530

    one final hope

--- app/controller/getSingleCert.tsx
+++ app/controller/getSingleCert.tsx
@@ -2,8 +2,8 @@ export default async function getSingleSubject(id: any) {
   try {
     console.log(id);
     const res = await fetch(
-      `https://r-alevel.netlify.app/api/certificates/${id}`,
-      // `https://alevel.netlify.app/certificates/${id}`,
+      // `https://r-alevel.netlify.app/api/certificates/${id}`,
+      `https://alevel.netlify.app/certificates/${id}`,
       // `http://localhost:3000/api/certificates/${id}`,
       {
         cache: "no-store",
